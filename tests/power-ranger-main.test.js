3bb9553034fed245d030931c5e058ddf1016f04a
Aamir <aamir.iqubal@tcs.com>
2019-05-12 11:31:05 +0300

    Add excess length field test

--- tests/power-ranger-main.test.js
+++ tests/power-ranger-main.test.js
@@ -3,23 +3,29 @@ import { Selector } from 'testcafe';
 fixture(`Here we go! Testing our Ranger`)
   .page(`http://localhost:3000/`);
 
-test('Power Ranger main page load test', async t => {
+test('Power Ranger main page load test.', async t => {
   await t.expect(Selector('title').innerText).eql('Power Ranger');
   await t.expect(Selector('.image-container').find('h1').innerText).eql('Power Ranger');
 });
 
-test('Test input field: with empty value : returns empty field error message', async t => {
+test('Test input field with empty value, returns empty field error message.', async t => {
   await t.click(Selector('.button-submit').withText('EVALUATE'));
   await t.expect(Selector('.ps-notification--content').innerText).eql('Input field cannot be empty.')
 });
 
-test('Test input field: with invalid values : returns invalid input value error message', async t => {
+test('Test input field with invalid values, returns invalid input value error message.', async t => {
   await t.typeText(Selector('.input_field').withAttribute('name', 'input'),'(0, invalid-coordinate)');
   await t.click(Selector('.button-submit').withText('EVALUATE'));
-  await t.expect(Selector('.ps-notification--content').innerText).eql('Input field should only contains number. value: "invalid-coordinate" not acceptable for input:(0, invalid-coordinate)')
+  await t.expect(Selector('.ps-notification--content').innerText).eql('Input field should only contain numbers. value: "invalid-coordinate" not acceptable. for input:(0, invalid-coordinate)')
 });
 
-test('Test input field: with proper values : returns calculated value in output field', async t => {
+test('Test input field with incorrect length of values, returns invalid input value error message.', async t => {
+  await t.typeText(Selector('.input_field').withAttribute('name', 'input'),'(0, 100, 12)');
+  await t.click(Selector('.button-submit').withText('EVALUATE'));
+  await t.expect(Selector('.ps-notification--content').innerText).eql('Improper x, y as input. Object length should be 2 for input:(0, 100, 12)')
+});
+
+test('Test input field with proper values, returns calculated value in output field', async t => {
   await t.typeText(Selector('.input_field').withAttribute('name', 'input'),'(0, 0) \n (100, 1)');
   await t.click(Selector('.button-submit').withText('EVALUATE'));
   await t.expect(Selector('.ps-notification--content').innerText).eql('Power ranger did it!')
